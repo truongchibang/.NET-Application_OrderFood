7fe9eba48b03c50d1ee464c9577f44b8fe901ac1
truongchibang <bangchitruong@gmail.com>
2025-04-16 19:36:48 +0700

    start project

---
+++ PRN212_PROJECT/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PRN212_PROJECT;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
